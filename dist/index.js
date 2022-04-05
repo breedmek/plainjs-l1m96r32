5054a9bda34a0e62220443f1cb5ec004f29daf25
Marco Antonio <65682825+undefined@users.noreply.github.com>
2022-04-05 14:50:48 +0000

    deploy Tue Apr 05 2022 14:50:48 GMT+0000 (Coordinated Universal Time)

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+import { packd_export_0 } from 'https://srv.divriots.com/packd/lwc@2.4.0';const { LightningElement } = packd_export_0;;
+export default class App extends LightningElement {
+  constructor(...args) {
+    super(...args);
+    this.message = 'Hello World';
+  }
+
+}
